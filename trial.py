ddb5236bdf88b99cc1e4037d80b686e1d333b7eb
Sarat Chandra <csarat48@gmail.com>
2023-10-02 19:17:56 +0530

    read all files in folder and plot

--- trial.py
+++ trial.py
@@ -21,7 +21,7 @@ for aircraft in json_data["aircraft"]:
       "track": pd.to_numeric(aircraft["track"]) if "track" in aircraft else np.nan,
       "track_rate": pd.to_numeric(aircraft["track_rate"]) if "track_rate" in aircraft else np.nan,
       "mach": pd.to_numeric(aircraft["mach"]) if "mach" in aircraft else np.nan,
-      "baro_rate": pd.to_numeric(aircraft["baro_rate"]) if "alt_geom" in aircraft else np.nan,
+      "baro_rate": pd.to_numeric(aircraft["baro_rate"]) if "baro_rate" in aircraft else np.nan,
       "nav_qnh": pd.to_numeric(aircraft["nav_qnh"]) if "nav_qnh" in aircraft else np.nan,
       "mag_heading": pd.to_numeric(aircraft["mag_heading"]) if "mag_heading" in aircraft else np.nan ,
       "nav_altitude_mcp": pd.to_numeric(aircraft["nav_altitude_mcp"]) if "nav_altitude_mcp" in aircraft else np.nan,
@@ -31,80 +31,124 @@ for aircraft in json_data["aircraft"]:
 
 # Create a Pandas DataFrame
 df = pd.DataFrame(flight_details)
+df["trk"] = (math.pi / 180) * df["track"]
+df["hdg"] = (math.pi / 180) * df["mag_heading"]
 
 # Print the DataFrame
 print(df)
 
-
-
-
-
 #%%
+import pandas as pd
+import numpy as np
+
+def calculate_wind_speed_and_direction(df):
+  """Calculates the wind speed and wind direction in meters per second and
+  degrees, respectively, from the given DataFrame.
+
+  Args:
+    df: A Pandas DataFrame containing the aircraft's flight details.
+
+  Returns:
+    A DataFrame containing the wind speed and wind direction for each row.
+  """
+
+  # Check if any of the "tas", "gs", "hdg", or "track" columns are NaN.
+  if df["tas"].isna().all() or df["trk"].isna().all() or df["hdg"].isna().all() or df["gs"].isna().all():
+    # If any of the columns are NaN, set the wind speed and wind direction to NaN.
+    df["ws"] = np.nan
+    df["wd"] = np.nan
+  else:
+    # Calculate the wind speed and wind direction.
+    df["ws"] = np.round(np.sqrt(np.power(df["tas"] - df["gs"], 2) + 4 * df["tas"] * df["gs"] * np.power(np.sin((df["hdg"] - df["trk"]) / 2), 2)))
+    df["wd"] = df["trk"] + np.arctan2(df["tas"] * np.sin(df["hdg"] - df["trk"]), df["tas"] * np.cos(df["hdg"] - df["trk"]) - df["gs"])
+
+  # Fix the wind direction if it is negative or greater than 360 degrees.
+  if df["wd"].any() < 0:
+    df["wd"] = df["wd"] + 2 * np.pi
+  if df["wd"].any() > 2 * np.pi:
+    df["wd"] = df["wd"] - 2 * np.pi
+
+  # Convert the wind direction from radians to degrees.
+  df["wd"] = np.round((180 / np.pi) * df["wd"])
+
+
+  # Check if any of the "tas", or "mach" columns are NaN.
+  if df["tas"].isna().all() or df["mach"].isna().all():
+    # If any of the columns are NaN, set the wind speed and wind direction to NaN.
+    df["oat"] = np.nan
+    df["tat"] = np.nan
+  else:
+    # Calculate the wind speed and wind direction.
+    df["oat"] = np.power((df["tas"] / 661.47 / df["mach"]), 2) * 288.15 - 273.15
+    df["tat"] = -273.15 + (df["oat"] + 273.15) * (1 + 0.2 * df["mach"] * df["mach"])
+
+  return df
+#%%
+# Calculate the wind speed, wind direction, oat and tat for each row of the DataFrame.
+df = calculate_wind_speed_and_direction(df.copy())
+
+# Print the DataFrame.
+print(df)
+
+# %%
+import pandas as pd
+import json
 import numpy as np
 import math
 
-def calculate_wind_speed_and_direction(aircraft):
-  """Calculates the wind speed and direction of the given aircraft.
+# Get a list of all the JSON files in the folder
+json_files = glob.glob('dump1090-fa/*.json')
 
-  Args:
-    aircraft: A dictionary containing the aircraft's flight details.
+# Create an empty list to store the Pandas DataFrames
+df_list = []
 
-  Returns:
-    A tuple containing the wind speed and direction in degrees.
-  """
+# Iterate over the list of JSON files and read each file into a Pandas DataFrame
+for json_file in json_files:
+    json_data = json.load(open(json_file))
 
-  # Preprocess the track and heading values.
+    # Check if the "aircraft" key exists in the JSON data
+    if "aircraft" in json_data:
+        # Create a list of flight details
+        flight_details = []
+        for aircraft in json_data["aircraft"]:
+            flight_details.append({
+                "hex": aircraft["hex"],
+                "flight": aircraft["flight"] if "flight" in aircraft else np.nan,
+                "alt_baro": pd.to_numeric(aircraft["alt_baro"]) if "alt_baro" in aircraft else np.nan,
+                "alt_geom": pd.to_numeric(aircraft["alt_geom"]) if "alt_geom" in aircraft else np.nan,
+                "gs": pd.to_numeric(aircraft["gs"]) if "gs" in aircraft else np.nan,
+                "tas": pd.to_numeric(aircraft["tas"]) if "tas" in aircraft else np.nan,
+                "track": pd.to_numeric(aircraft["track"]) if "track" in aircraft else np.nan,
+                "track_rate": pd.to_numeric(aircraft["track_rate"]) if "track_rate" in aircraft else np.nan,
+                "mach": pd.to_numeric(aircraft["mach"]) if "mach" in aircraft else np.nan,
+                "baro_rate": pd.to_numeric(aircraft["baro_rate"]) if "baro_rate" in aircraft else np.nan,
+                "nav_qnh": pd.to_numeric(aircraft["nav_qnh"]) if "nav_qnh" in aircraft else np.nan,
+                "mag_heading": pd.to_numeric(aircraft["mag_heading"]) if "mag_heading" in aircraft else np.nan ,
+                "nav_altitude_mcp": pd.to_numeric(aircraft["nav_altitude_mcp"]) if "nav_altitude_mcp" in aircraft else np.nan,
+                "lat": aircraft["lat"] if "lat" in aircraft else np.nan,
+                "lon": aircraft["lon"] if "lon" in aircraft else np.nan,
+            })
 
-  trk = (math.pi / 180) * aircraft["track"]
-  hdg = (math.pi / 180) * aircraft["mag_heading"]
+        # Create a Pandas DataFrame
+        df = pd.DataFrame(flight_details)
+        df["trk"] = (math.pi / 180) * df["track"]
+        df["hdg"] = (math.pi / 180) * df["mag_heading"]
 
-  # Calculate the true airspeed (TAS) in meters per second.
-  tas = aircraft["tas"]
+        # Append the Pandas DataFrame to the empty list
+        df_list.append(df)
 
-  # Calculate the ground speed (GS) in meters per second.
-  gs = aircraft["gs"]
+# Concatenate all the Pandas DataFrames in the list into a single Pandas DataFrame
+df = pd.concat(df_list)
 
-  # Handle the NaN values.
-  if np.isnan(trk) or np.isnan(hdg):
-    ws = np.nan
-    wd = np.nan
-  else:
-    # Calculate the wind speed in meters per second.
-    ws = round(math.sqrt(math.pow(tas - gs, 2) + 4 * tas * gs * math.pow(math.sin((hdg - trk) / 2), 2)))
-
-    # Calculate the wind direction in degrees.
-    wd = trk + math.atan2(tas * math.sin(hdg - trk), tas * math.cos(hdg - trk) - gs)
-
-    # Fix the wind direction if it is negative or greater than 360 degrees.
-    if wd < 0:
-      wd = wd + 2 * math.pi
-    if wd > 2 * math.pi:
-      wd = wd - 2 * math.pi
-
-    # Convert the wind direction from radians to degrees.
-    wd = round((180 / math.pi) * wd)
-
-  return ws, wd
+# Print the Pandas DataFrame
+print(df)
 
 
-
-
-#%%
-
-# Create a Pandas DataFrame from the flight details.
-flight_df = df
-
-# Calculate the wind speed and direction for each aircraft.
-for i in range(len(flight_df)):
-    
-
-    # Add the wind speed and direction to the DataFrame.
-    flight_df.loc[i, "wind_speed"] = wind_speed
-    flight_df.loc[i, "wind_direction"] = wind_direction
-
-# Print the DataFrame.
-print(flight_df)
 
 # %%
-result = df.apply(calculate_wind_speed_and_direction, axis=1)
+import matplotlib.pyplot as plt
+plt.scatter(df.ws,df.alt_geom)
+#%%
+plt.scatter(df.oat,df.alt_geom)
+
 # %%
