2050a4fabdd080f9e973326f902f951f89b44d94
carbform <csarat48@gmail.com>
2023-10-05 10:49:31 +0530

    new josn files

--- dump1090-fa/get_adsb.sh
+++ dump1090-fa/get_adsb.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# You can use this code to copy all the .json files remotely from your Raspberry Pi / any Linux desktop running dump1090-fa.
-# Replace the username and the IP address with the remote machine's.
-# Also, change the destination folder /home/user/aircraft to suit your's appropriately.
-scp -r username@xx.xxx.xx.xx:/var/run/dump1090-fa/*.json /home/user/aircraft
-
-
+scp -r carbform-pi@10.197.12.30:/var/run/dump1090-fa/*.json /home/carbform/Documents/daily_dump
