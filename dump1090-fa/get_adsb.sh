ffa3965a110e8244dcb5c87de83e426fc5aa9f4c
carbform <csarat48@gmail.com>
2023-10-16 10:18:33 +0530

    new json files

--- dump1090-fa/get_adsb.sh
+++ dump1090-fa/get_adsb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -r carbform-pi@10.197.12.30:/var/run/dump1090-fa/*.json /home/carbform/Documents/daily_dump
+scp -r carbform-pi@10.197.12.30:/var/run/dump1090-fa/*.json /home/carbform/meto_adsb/dump1090-fa
