8ffda2c580fc31a1aa238f1e7d9cf106e42f4aa2
carbform <csarat48@gmail.com>
2023-10-31 12:12:25 +0530

    added basemaps

--- script.js
+++ script.js
@@ -87,7 +87,80 @@ function calculateWindSpeedAndDirection(df) {
 
   return [filteredWindData, filteredTemperatureData];
 }
+let map;
+let mapCenter = [13, 77.5];
+let mapZoom = 9;
 
+function extractLatLonAndMap(aircraftData) {
+  // Filter out data with missing or undefined latitude or longitude values
+  const validLocations = aircraftData.filter((row) => {
+    return typeof row.lat === 'number' && typeof row.lon === 'number';
+  });
+
+  if (validLocations.length === 0) {
+    console.error("No valid latitude and longitude data found in aircraftData.");
+    return;
+  }
+
+  if (!map) {
+    // Create a map centered at the initial map center and zoom
+    map = L.map("map").setView(mapCenter, mapZoom);
+
+    // Add a tile layer (basemap)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Get the latitude, longitude, and zoom input elements
+    const latInput = document.getElementById('map-lat-input');
+    const lonInput = document.getElementById('map-lon-input');
+    const zoomInput = document.getElementById('map-zoom-input');
+
+    // Add event listeners to update map center and zoom when inputs change
+    latInput.addEventListener('input', () => {
+      const lat = parseFloat(latInput.value);
+      mapCenter[0] = isNaN(lat) ? 0 : lat;
+      map.setView(mapCenter, mapZoom);
+    });
+
+    lonInput.addEventListener('input', () => {
+      const lon = parseFloat(lonInput.value);
+      mapCenter[1] = isNaN(lon) ? 0 : lon;
+      map.setView(mapCenter, mapZoom);
+    });
+
+    zoomInput.addEventListener('input', () => {
+      const zoom = parseInt(zoomInput.value);
+      mapZoom = isNaN(zoom) ? 9 : zoom;
+      map.setView(mapCenter, mapZoom);
+    });
+  }
+
+  // Define a function to determine the color based on some criteria
+  function getColor() {
+    // You can define your own logic to determine the color based on data
+    // For simplicity, we'll use a random color here
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  }
+
+  // Clear existing layers from the map
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Circle) {
+      map.removeLayer(layer);
+    }
+  });
+
+  // Add colored points to the map based on latitude and longitude data
+  validLocations.forEach((location) => {
+    L.circle([location.lat, location.lon], {
+      color: getColor(),
+      fillColor: getColor(),
+      fillOpacity: 0.8,
+      radius: 500,
+    }).addTo(map);
+  });
+}
 
 // Function to create a Chart.js scatter plot with custom aspect ratio
 function createScatterChart(chartId, chartData, chartTitle, xLabel, yLabel, xMin, xMax, yMin, yMax, color, aspectRatio) {
@@ -228,6 +301,7 @@ chart2 = createScatterChart(
     chart2.options.scales.x.grid.color = getComputedStyle(document.documentElement).getPropertyValue('--chart-grid-color');
     chart2.options.scales.y.grid.color = getComputedStyle(document.documentElement).getPropertyValue('--chart-grid-color');
   }
+  extractLatLonAndMap(aircraftData);
 }
 
 // Call the main function.
@@ -271,9 +345,15 @@ function updateChartScales(chartId, xMinId, xMaxId, yMinId, yMaxId) {
   }
 }
 
+// Define a dark mode tile layer
+const darkModeTileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+
 function toggleDarkMode() {
   const body = document.body;
   const chartForms = document.querySelectorAll('.form-container');
+  
 
   if (body.classList.contains('light-mode')) {
     body.classList.replace('light-mode', 'dark-mode');
